52156d4451c7895919d61df86afb87d4e16b1e5e
Todd Brandt <tebrandt@frontier.com>
2013-11-07 04:20:40 +0000

    updated make_deb

--- scripts/make_deb.sh
+++ scripts/make_deb.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 # update via SVN
-svn up
+#svn up
 
 # find out the current revision
-SVNVERSION="$(export LANG=C && export LC_ALL=C && echo $(svn info | awk '/^Revision:/ { print $2 }'))"
+#SVNVERSION="$(export LANG=C && export LC_ALL=C && echo $(svn info | awk '/^Revision:/ { print $2 }'))"
 
 # use checkinstall to create the DEB package
 sudo checkinstall -D \
                   --pkgname "mjpg-streamer" \
-                  --pkgversion "r$SVNVERSION" \
+                  --pkgversion "1.0" \
                   --pkgrelease "1" \
-                  --maintainer "tom_stoeveken@users.sourceforge.net" \
+                  --maintainer "tebrandt@frontier.com" \
                   --requires "libjpeg62" \
                   --nodoc \
                     make DESTDIR=/usr install
